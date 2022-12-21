85c61308c57ae51f8068d1863999dc7863473062
Adri <adrien.gaudon@liveblocks.io>
2022-12-21 13:55:54 -0500

    remove whiteboard

--- layouts/Documents/Documents.tsx
+++ layouts/Documents/Documents.tsx
@@ -138,9 +138,9 @@ export function DocumentsLayout({
             initialValue="all"
             items={[
               { value: "all", title: "All" },
-              { value: "sitcom", title: "Sitcom", disabled: true },
+              { value: "sitcom", title: "Sitcom" },
               { value: "play", title: "Play" },
-              { value: "movie", title: "Movie", disabled: true },
+              { value: "movie", title: "Movie" },
             ]}
             onChange={(value: "all" | DocumentType) => {
               setDocumentType(value);
