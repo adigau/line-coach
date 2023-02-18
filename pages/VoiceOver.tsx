a6f9e21d1a41bd4e5a4a54ce31c8344a0b0cc71a
Adri <adrien.gaudon@liveblocks.io>
2023-02-18 10:43:52 -0500

    init file

---
+++ pages/VoiceOver.tsx
@@ -0,0 +1,28 @@
+
+export default function VoiceOver() {
+    return (<>hello</>)
+}
+
+
+// fr	French
+// fr-BE	French (Belgium)
+// fr-CA	French (Canada)
+// fr-CH	French (Switzerland)
+// fr-FR	French (France)
+// fr-LU	French (Luxembourg)
+// fr-MC	French (Principality of Monaco)
+
+// en	English
+// en-AU	English (Australia)
+// en-BZ	English (Belize)
+// en-CA	English (Canada)
+// en-CB	English (Caribbean)
+// en-GB	English (United Kingdom)
+// en-IE	English (Ireland)
+// en-JM	English (Jamaica)
+// en-NZ	English (New Zealand)
+// en-PH	English (Republic of the Philippines)
+// en-TT	English (Trinidad and Tobago)
+// en-US	English (United States)
+// en-ZA	English (South Africa)
+// en-ZW	English (Zimbabwe)
